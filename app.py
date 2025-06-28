a62104088230267f281d5915796c3add58cc358c
Scal68128 <pascal68128@icloud.com>
2025-06-28 10:22:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    data = request.json
-    print("Signal reçu :", data)  # Affiche le contenu dans les logs
-    return 'OK'
+    if request.is_json:
+        data = request.get_json()
+        print("Signal reçu :", data)
+        return jsonify({"status": "ok"}), 200
+    else:
+        print("⚠️ Requête non-JSON reçue")
+        return jsonify({"error": "Unsupported Media Type"}), 415
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
